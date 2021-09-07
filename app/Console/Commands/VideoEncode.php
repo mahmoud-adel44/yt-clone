5c554b2123561452910f55a4aafc9843f04a0c79
mahmoud-adel44 <mhmoud.adel18@yahoo.com>
2021-09-08 01:09:20 +0200

    second commit

---
+++ app/Console/Commands/VideoEncode.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use FFMpeg\Format\Video\X264;
+use Illuminate\Console\Command;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
+
+class VideoEncode extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'video-encode:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Video Encoding...';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $low = (new x264('aac'))->setKiloBitrate(500);
+        $high = (new x264('aac'))->setKiloBitrate(1000);
+
+        FFMpeg::fromDisk('videos-temp')
+            ->open('file.mkv')
+            ->exportForHLS()
+//            ->addFormat($low , function($filter){
+//                $filter->resize(640 , 480);
+//            })
+//            ->addFormat($high , function($filter){
+//                $filter->resize(1200 , 720);
+//            })
+            ->onProgress(function($progress){
+            $this->info("progress={$progress}");
+            })
+            ->addFormat($low)
+            ->addFormat($high)
+            ->toDisk('videos-temp')
+            ->save('/test/file.m3u8');
+
+    }
+}
